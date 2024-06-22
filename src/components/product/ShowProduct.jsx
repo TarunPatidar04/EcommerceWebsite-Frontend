d1f6abfc5d11d79b54ccd1407d209442c0acf586
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-06-22 12:28:00 +0530

    create a show Products

--- src/components/product/ShowProduct.jsx
+++ src/components/product/ShowProduct.jsx
@@ -6,11 +6,46 @@ const ShowProduct = () => {
   return (
     <>
       <h1>Show Product</h1>
-      {products?.map((product) => (
-        <div key={product._id}>
-          <h2>{product.title}</h2>
+      <div className="container d-flex justify-content-center align-items-center">
+        <div className="row container d-flex justify-content-center align-items-center">
+          {products?.map((product) => (
+            <div
+              key={product._id}
+              className=" my-3 col-md-4 d-flex justify-content-center align-items-center"
+            >
+              <div
+                className="card bg-dark text-light text-center"
+                style={{ width: "18rem" }}
+              >
+                <div className="d-flex justify-content-center align-items-center p-3">
+                  <img
+                    src={product.imgSrc}
+                    className="card-img-top"
+                    alt="..."
+                    style={{
+                      width: "200px",
+                      height: "200px",
+                      borderRadius: "10px",
+                      border: "2px solid yellow",
+                    }}
+                  />
+                </div>
+                <div className="card-body">
+                  <h5 className="card-title">{product.title}</h5>
+                  <div className="my-3">
+                    <button href="#" className="btn btn-primary mx-3">
+                      {product.price} {"₹"}
+                    </button>
+                    <button href="#" className="btn btn-warning">
+                      Add to cart
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </>
   );
 };
