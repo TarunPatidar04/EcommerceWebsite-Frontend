e69c2e0a3da6ab9bab7bbbbcdf3a040d1202c42a
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-06-23 11:04:07 +0530

    Create a Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React, { useContext } from "react";
 import ShowProduct from "./components/product/ShowProduct";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ProductDetail from "./components/product/ProductDetail";
+import Navbar from "./components/Navbar";
 const App = () => {
   return (
     <>
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<ShowProduct />} />
           <Route path="/product/:id" element={<ProductDetail />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
-      Navbar
+      <div className="nav sticky-top">
+        <div className="nav_bar">
+          <div className="left">
+            <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+              <h3>Ecommerce</h3>
+            </Link>
+          </div>
+          <div className="search_bar">
+            <span className="material-symbols-outlined">search</span>
+            <input type="text" placeholder="Search Products..." />
+          </div>
+          <div className="right">
+            <button className="btn btn-warning mx-3">cart</button>
+            <button className="btn btn-warning mx-3">profile</button>
+            <button className="btn btn-warning mx-3">login</button>
+            <button className="btn btn-warning mx-3">regsiter</button>
+            <button className="btn btn-warning mx-3">logout</button>
+          </div>
+        </div>
+        <div className="sub_bar"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
