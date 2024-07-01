9cd7fcdb254d0df1f69ccb789a770e7f27a107f6
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-07-01 13:18:58 +0530

    create a Confirmation Page

--- src/components/OrderConfirmation.jsx
+++ src/components/OrderConfirmation.jsx
@@ -1,14 +1,30 @@
-import { useContext } from "react"
-import AppContext from "../context/AppContext"
-
+import { useContext, useEffect, useState } from "react";
+import AppContext from "../context/AppContext";
 
 const OrderConfirmation = () => {
-  const { userOrder } = useContext(AppContext)
+  const { userOrder } = useContext(AppContext);
+  const [latestOrder, setLatestOrder] = useState({});
+  useEffect(() => {
+    if (userOrder) {
+      setLatestOrder(userOrder[0]);
+    }
+  }, [userOrder]);
+  // console.log("latest order",latestOrder)
   return (
-    <div style={{marginTop:"110px"}}>
-      orderconfirmation
+    <div style={{ marginTop: "110px" }}>
+      <div>
+        <div className="container my-5">
+          <h1 className="text-center">Your order has been confirmed</h1>
+          <h3 className="text-center">It will delivered soon</h3>
+        </div>
+        <div>
+          <div>
+           
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default OrderConfirmation
+export default OrderConfirmation;
